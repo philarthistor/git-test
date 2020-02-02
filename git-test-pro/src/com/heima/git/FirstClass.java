9eeec33f58ae4d7930a8ccdb812f9470476bf0ec
jjk <116096880@qq.com>
2020-02-02 13:04:42 +0800

    wdgsa

---
+++ git-test-pro/src/com/heima/git/FirstClass.java
@@ -0,0 +1,14 @@
+package com.heima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+        System.out.println("test git project");
+        System.out.println("test git project");
+        System.out.println("test git project");
+        System.out.println("test git project");
+        System.out.println("test git project");
+        System.out.println("test git project");
+        System.out.println("test git project");
+    }
+}
